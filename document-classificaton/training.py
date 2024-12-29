e1d45e220d1a2b4960c2ad832f4eead142cddfd7
Kartik Sharma <karsharma10@gmail.com>
2024-12-29 09:17:13 -0700

    first round of finetuning bert for doc classification

---
+++ document-classificaton/training.py
@@ -0,0 +1,25 @@
+import os
+import shutil
+
+import tensorflow as tf
+import tensorflow_hub as hub
+import tensorflow_text as text
+
+import matplotlib.pyplot as plt
+
+
+
+bert_model_name = 'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-4_H-512_A-8/1'
+tfhub_handle_preprocess = 'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3'
+
+bert_preprocess_model = hub.KerasLayer(tfhub_handle_preprocess)
+
+
+text_test = ['this is such an amazing movie!']
+text_preprocessed = bert_preprocess_model(text_test)
+
+print(f'Keys       : {list(text_preprocessed.keys())}')
+print(f'Shape      : {text_preprocessed["input_word_ids"].shape}')
+print(f'Word Ids   : {text_preprocessed["input_word_ids"][0, :12]}')
+print(f'Input Mask : {text_preprocessed["input_mask"][0, :12]}')
+print(f'Type Ids   : {text_preprocessed["input_type_ids"][0, :12]}')
